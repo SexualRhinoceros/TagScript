ed89c00ef5ad389af5f64fa70efa4b1d7259f8b1
Rhino <SexualRhinoceros@users.noreply.github.com>
2017-09-07 08:07:06 -0700

    handles errors
    ayy

--- TagScriptEngine/math.py
+++ TagScriptEngine/math.py
@@ -1,5 +1,7 @@
 import re, ast
 
+from .exceptions import TagEngineError
+
 REGEX = re.compile("[mM]{(.+[^}])}")
 
 class MathEvaluationFilter():
@@ -11,8 +13,13 @@ class MathEvaluationFilter():
             return value # Exit early if there is no math expressions to begin with
 
         while match is not None:
-            evaluation = ast.literal_eval(match.group(1))
-            value = value.replace(match.group(0), str(evaluation))
-            match = REGEX.search(value)
+            for matched_group in match.groups():
+                final_value = self.Process(self, engine, matched_group)
+                try:
+                    evaluation = ast.literal_eval(match.group(1))
+                except Exception as e:
+                    raise TagEngineError('{}: {}\nMATH EXPRESSION: {}'.format(type(e).__name__, str(e), match.group(1)))
+                value = value.replace(match.group(0), str(evaluation))
+                match = REGEX.search(value)
 
         return value
